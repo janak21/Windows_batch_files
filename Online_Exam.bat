159aa87bbb9a60e75a6d567921adf46d4ab75a0e
janak21 <sawalejanak5@gmail.com>
2020-10-18 20:35:05 +0530

    Initial files

---
+++ Online_Exam.bat
@@ -0,0 +1,3 @@
+@echo off
+title Exam portal
+start "" https://sppu.wheebox.com/LOGIN-2/sppu.jsp
---
+++ Web.bat
@@ -0,0 +1,4 @@
+@echo off
+title Just testing!
+start "" https://mail.google.com/mail/u/0/?tab=rm&ogbl
+start "" https://web.whatsapp.com/
